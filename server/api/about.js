0935362b49fd68f3eece940a8502655ac459c1cd
KdompySaing <kdompy.saing@wooribank.com.kh>
2024-12-09 09:41:43 +0700

    Revert "update profile"
    This reverts commit 68fc9036ef23da210b6eb9b69f465e4c0836d398.

--- server/api/about.js
+++ server/api/about.js
@@ -5,8 +5,8 @@ export default defineEventHandler(() => {
     {
       id: 1,
       description: {
-        en: 'A self-motivated and curious being with over 10 years of experience in making quality Mobile Applications and System Development that adhere to core  design principles. Adept at prioritizing and completing tasks to meet business requirements. A team player who is attentive to detail and able to work in a fast-paced environment. Excellent oral and written communication skills.',
-     id_ID: 'A self-motivated and curious being with over 10 years of experience in making quality Mobile Applications and System Development that adhere to core  design principles. Adept at prioritizing and completing tasks to meet business requirements. A team player who is attentive to detail and able to work in a fast-paced environment. Excellent oral and written communication skills..',
+        en: 'A self-motivated and curious being with over 10 years of experience in making quality mobile applications that adhere to core  design principles. Adept at prioritizing and completing tasks to meet business requirements. A team player who is attentive to detail and able to work in a fast-paced environment. Excellent oral and written communication skills.',
+     id_ID: 'A self-motivated and curious being with over 10 years of experience in making quality mobile applications that adhere to core  design principles. Adept at prioritizing and completing tasks to meet business requirements. A team player who is attentive to detail and able to work in a fast-paced environment. Excellent oral and written communication skills..',
       },
     },
   ]
